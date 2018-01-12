18a7c054846906f63155713e8ec79db3c2edcec8
atarvainen <33045386+atarvainen@users.noreply.github.com>
2018-01-12 16:28:23 +0200

    added T6v2

---
+++ t6v2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Tehtävä 6
+
+Auton kulutus on 7.02 litraa 100 kilometrin matkalla ja bensan hinta on 1.595 Euroa. Tee ohjelma, joka tulostaa ajetulla matkalla (kysytään käyttäjältä) kuluvan bensan määrän sekä bensaan menevän rahan määrän.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give distance: ");
+            string line = Console.ReadLine();
+            double km = 0;
+            double mileage = 0.0702;
+            double gasprice = 1.595;
+            if (double.TryParse(line, out km))
+            {
+                double consume = km * mileage;
+                double price = consume * gasprice;
+                Console.WriteLine("Gasoline consume is " + consume + " liters and cost is " + price + " euros.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Not a valid distance.");
+            }
+        }
+    }
+}
