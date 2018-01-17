425c153d6db18c7bb1c3611df05d91fefdf919eb
L4623 <L4623@student.jamk.fi>
2018-01-17 16:42:37 +0200

    Added T3vko3

---
+++ t3vko4/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t3vko4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teevee samsung = new Teevee();
+            while (true)
+            {
+                Console.Write("Turn tv on? yes/no: ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "yes")
+                {
+                    samsung.Poweron();
+                    while (true)
+                    {
+                        Console.WriteLine("Change channel 1");
+                        Console.WriteLine("Change settings 2");
+                        Console.WriteLine("Turn off 3");
+                        int choice = int.Parse(Console.ReadLine());
+                        switch (choice)
+                        {
+                            case 1:
+                                {
+                                    Console.WriteLine("Channel up   =   1");
+                                    Console.WriteLine("Channel down =   2");
+                                    Console.WriteLine("Quit         =   3");
+                                    int channel = int.Parse(Console.ReadLine());
+                                    switch (channel)
+                                    {
+                                        case 1: samsung.ChannelUp(); continue;
+                                        case 2: samsung.ChannelDown(); continue;
+                                        case 3: break;
+                                    }
+                                }
+                                continue;
+                            case 2:
+                                {
+                                    Console.WriteLine("Adjust contrast   =   1");
+                                    Console.WriteLine("Adjust brightness =   2");
+                                    Console.WriteLine("Quit              =   3");
+                                    int setting = int.Parse(Console.ReadLine());
+                                    switch (setting)
+                                    {
+                                        case 1:
+                                            {
+                                                Console.WriteLine("Contrast up   =   1");
+                                                Console.WriteLine("Contrast down =   2");
+                                                Console.WriteLine("Quit          =   3");
+                                                int contrast = int.Parse(Console.ReadLine());
+                                                switch (contrast)
+                                                {
+                                                    case 1: samsung.ContrastUp(); continue;
+                                                    case 2: samsung.ContrastDown(); continue;
+                                                    case 3: break;
+                                                }
+                                            }
+                                            continue;
+                                        case 2:
+                                            {
+                                                Console.WriteLine("Brightness up   =   1");
+                                                Console.WriteLine("Brightness down =   2");
+                                                Console.WriteLine("Quit            =   3");
+                                                int brightness = int.Parse(Console.ReadLine());
+                                                switch (brightness)
+                                                {
+                                                    case 1: samsung.BrightnessUp(); break;
+                                                    case 2: samsung.BrightnessDown(); break;
+                                                    case 3: break;
+                                                }
+                                            }
+                                            continue;
+                                    }
+                                    continue;
+                                }
+                            case 3: break;
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ t3vko4/TV.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t3vko4
+{
+    class TV
+    {
+        public int Channel { get; set; }
+        public int Contrast { get; set; }
+        public int Brightness { get; set; }
+        public bool Power { get; set; }
+
+        public TV()
+        {
+            Channel = 1;
+            Brightness = 100;
+            Contrast = 50;
+            Power = false;
+        }
+
+        public void ChannelUp()
+        {
+            Channel += 1;
+            Console.WriteLine("Channel changed to {0}", Channel);
+        }
+
+        public void ChannelDown()
+        {
+            Channel -= 1;
+            Console.WriteLine("Channel changed to {0}", Channel);
+        }
+
+        public void ContrastUp()
+        {
+            Contrast += 5;
+            Console.WriteLine("Channel changed to {0}", Contrast);
+        }
+
+        public void ContrastDown()
+        {
+            Contrast -= 5;
+            Console.WriteLine("Channel changed to {0}", Contrast);
+        }
+
+        public void BrightnessUp()
+        {
+            Brightness += 5;
+            Console.WriteLine("Channel changed to {0}", Brightness);
+        }
+
+        public void BrightnessDown()
+        {
+            Brightness -= 5;
+            Console.WriteLine("Channel changed to {0}", Brightness);
+        }
+
+        public void Poweron()
+        {
+            Power = true;
+            Console.WriteLine("TV turned on");
+        }
+
+        public void Poweroff()
+        {
+            Power = false;
+            Console.WriteLine("TV turned off");
+        }
+    }
+}
