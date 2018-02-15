e608adf3eaa39af0a99b225ea668d470a7ce90b7
L4623 <L4623@student.jamk.fi>
2018-02-15 16:12:48 +0200

    made some changes to vko7t5

--- vko7to/t5/Program.cs
+++ vko7to/t5/Program.cs
@@ -19,50 +19,80 @@ namespace t5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void ReadFile(List<TVShow> tvshows)
         {
             try
-            {
-                List<TVShow> tvshows = new List<TVShow>();
+	        {
+                IFormatter formatter = new BinaryFormatter();
+
+		        Stream openStream = new FileStream("TV_Shows.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+                    
+                tvshows = (List<TVShow>)formatter.Deserialize(openStream);
+
+                openStream.Close();
+
+                foreach (TVShow show in tvshows)
+                {
+                    Console.WriteLine("Show name: {0}, Channel: {1}, Starting time: {2}, Ending time: {3}, Info: {4}", show.ShowName, show.Channel, show.StartingTime, show.EndingTime, show.Info);
+                }
+	        }
+
+	        catch (Exception)
+	        {
+
+		        throw;
+	        }
+        }
+
+        static void SaveToFile(List<TVShow> tvshows)
+        {
+            try
+	        {	        
+		        tvshows.Add(new TVShow { ShowName = "Frendit", Channel = "Sub", StartingTime = new DateTime(2018, 6, 1, 12, 30, 00), EndingTime = new DateTime(2018, 6, 1, 13, 00, 00), Info = "Sarja kaveruksista" });
+                tvshows.Add(new TVShow { ShowName = "Simpsons", Channel = "Mtv3", StartingTime = new DateTime(2018, 6, 1, 14, 30, 00), EndingTime = new DateTime(2018, 6, 1, 15, 00, 00), Info = "Sarja keltaisista hepuista" });
+                tvshows.Add(new TVShow { ShowName = "Futurama", Channel = "Nelonen", StartingTime = new DateTime(2018, 6, 1, 15, 30, 00), EndingTime = new DateTime(2018, 6, 1, 16, 00, 00), Info = "Sarja Simpsonien tekijöiltä" });
 
                 IFormatter formatter = new BinaryFormatter();
 
+                Stream writeMultipleStream = new FileStream("TV_Shows.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+
+                formatter.Serialize(writeMultipleStream, tvshows);
+
+                writeMultipleStream.Close();
+
+                foreach (TVShow show in tvshows)
+                {
+                    Console.WriteLine("Show name: {0}, Channel: {1}, Starting time: {2}, Ending time: {3}, Info: {4}", show.ShowName, show.Channel, show.StartingTime, show.EndingTime, show.Info);
+                }
+	        }
+
+	        catch (Exception)
+	        {
+
+		        throw;
+	        }
+        }
+
+        static void Main(string[] args)
+        {
+            List<TVShow> tvshows = new List<TVShow>();
+
+            try
+            {
                 if (File.Exists("TV_Shows.bin"))
                 {
-                    Stream openStream = new FileStream("TV_Shows.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
-                    
-                    tvshows = (List<TVShow>)formatter.Deserialize(openStream);
-
-                    openStream.Close();
-
-                    foreach (TVShow show in tvshows)
-                    {
-                        Console.WriteLine("Show name: {0}, Channel: {1}, Starting time: {2}, Ending time: {3}, Info: {4}", show.ShowName, show.Channel, show.StartingTime, show.EndingTime, show.Info);
-                    }
+                    ReadFile(tvshows);
                 }
 
                 else
                 {
-                    tvshows.Add(new TVShow { ShowName = "Frendit", Channel = "Sub", StartingTime = new DateTime(2018, 6, 1, 12, 30, 00), EndingTime = new DateTime(2018, 6, 1, 13, 00, 00), Info = "Sarja kaveruksista" });
-                    tvshows.Add(new TVShow { ShowName = "Simpsons", Channel = "Mtv3", StartingTime = new DateTime(2018, 6, 1, 14, 30, 00), EndingTime = new DateTime(2018, 6, 1, 15, 00, 00), Info = "Sarja keltaisista hepuista" });
-                    tvshows.Add(new TVShow { ShowName = "Futurama", Channel = "Nelonen", StartingTime = new DateTime(2018, 6, 1, 15, 30, 00), EndingTime = new DateTime(2018, 6, 1, 16, 00, 00), Info = "Sarja Simpsonien tekijöiltä" });
-
-                    Stream writeMultipleStream = new FileStream("TV_Shows.bin", FileMode.Create, FileAccess.Write, FileShare.None);
-
-                    formatter.Serialize(writeMultipleStream, tvshows);
-
-                    writeMultipleStream.Close();
-
-                    foreach (TVShow show in tvshows)
-                    {
-                        Console.WriteLine("Show name: {0}, Channel: {1}, Starting time: {2}, Ending time: {3}, Info: {4}", show.ShowName, show.Channel, show.StartingTime, show.EndingTime, show.Info);
-                    }
+                    SaveToFile(tvshows);
                 }
             }
 
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                Console.WriteLine(ex);
             }
 
             Console.ReadKey();
